5a86dd2ccf3a52ed4c602dcb5be80f6dcab15d35
The Doctor <learicist@gmail.com>
2018-07-07 17:15:52 -0400

    day 2 of project, 7.7.18

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const arr = [
 	{name: 'giroux', tag: $('#giroux'), vidTag: $('#girouxReel'), row: 'top'},
 	{name: 'wayne', tag: $('#wayne'), vidTag: $('#wayneReel'), row: 'top'},
 	{name: 'hasek', tag: $('#hasek'), vidTag: $('#hasekReel'), row: 'top'},
-	{name: 'dualPraise', tag: $('#dualPraise'), vidTag: $('#dualPraiseReel'), row: 'mid'},
+	{name: 'mario', tag: $('#mario'), vidTag: $('#marioReel'), row: 'mid'},
 	{name: 'marty', tag: $('#marty'), vidTag: $('#martyReel'), row: 'mid'},
 	{name: 'roy', tag: $('#roy'), vidTag: $('#royReel'), row: 'mid'},
 	{name: 'yzerman', tag: $('#yzerman'), vidTag: $('#yzermanReel'), row: 'low'},
@@ -11,44 +11,59 @@ const arr = [
 ];
 
 $(document).ready(function () {
+	
+	$('#title').on({
+		'mouseenter': function () {
+			$(this).css('cursor', 'pointer');
+		},
+		'click': function () {
+			location.reload();
+		}
+	});
+	
 	//main handler for user interactions
-	$('.pics').on('click', function () {
-		let curr = this.id;
-		console.log(curr);
-		for (let i in arr) {
-			if (curr == arr[i].name) {
-				//hide clicked picture and reveal video
-				arr[i].tag.addClass('hidden');
-				arr[i].vidTag.removeClass('hidden');
-				let row = arr[i].row;
-				if (row == 'top') {
-					$('#topRowReturn').removeClass('hidden');
-					//hide the rest of the row
-					for (let j in arr) {
-						if (arr[j].row == row) {
-							arr[j].tag.addClass('hidden');
+	$('.pics').on({
+		'mouseenter': function () {
+			$(this).css('cursor', 'pointer');
+		},		
+		'click': function () {
+			let curr = this.id;
+			console.log(curr);
+			for (let i in arr) {
+				if (curr == arr[i].name) {
+					//hide clicked picture and reveal video
+					arr[i].tag.addClass('hidden');
+					arr[i].vidTag.removeClass('hidden');
+					let row = arr[i].row;
+					if (row == 'top') {
+						$('#topRowReturn').removeClass('hidden');
+						//hide the rest of the row
+						for (let j in arr) {
+							if (arr[j].row == row) {
+								arr[j].tag.addClass('hidden');
+							}
 						}
-					}
-				} else if (row == 'mid') {
-					$('#midRowReturn').removeClass('hidden');
-					//hide the rest of the row
-					for (let j in arr) {
-						if (arr[j].row == row) {
-							arr[j].tag.addClass('hidden');
+					} else if (row == 'mid') {
+						$('#midRowReturn').removeClass('hidden');
+						//hide the rest of the row
+						for (let j in arr) {
+							if (arr[j].row == row) {
+								arr[j].tag.addClass('hidden');
+							}
 						}
-					}
-				} else if (row == 'low') {
-					$('#lowRowReturn').removeClass('hidden');
-					//hide the rest of the row
-					for (let j in arr) {
-						if (arr[j].row == row) {
-							arr[j].tag.addClass('hidden');
+					} else if (row == 'low') {
+						$('#lowRowReturn').removeClass('hidden');
+						//hide the rest of the row
+						for (let j in arr) {
+							if (arr[j].row == row) {
+								arr[j].tag.addClass('hidden');
+							}
 						}
-					}
-				}//end of internal if statement
-			}//end of if statement
-		}//end of for loop
-	});//end of click function
+					}//end of internal if statement
+				}//end of if statement
+			}//end of for loop
+		}//end of click function
+	});
 	
 	//behavior of the return buttons
 	$('#topRowReturn').click(function () {

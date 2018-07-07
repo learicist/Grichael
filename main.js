c228f942544486de17a9191a3aefb851ea9ffd7a
The Doctor <learicist@gmail.com>
2018-07-07 02:36:36 -0400

    Object functionality, some CSS. Last commit on night of page origin - 7.6.18

--- main.js
+++ main.js
@@ -1,81 +1,62 @@
 const arr = [
-	{name: 'giroux', tag: $('#giroux'), vidTag: $('#girouxReel')},
-	{name: 'wayne', tag: $('#wayne'), vidTag: $('#wayneReel')},
-	{name: 'hasek', tag: $('#hasek'), vidTag: $('#hasekReel')},
-	{name: 'dualPraise', tag: $('#dualPraise'), vidTag: $('#dualPraiseReel')},
-	{name: 'marty', tag: $('#marty'), vidTag: $('#martyReel')},
-	{name: 'roy', tag: $('#roy'), vidTag: $('#royReel')},
-	{name: 'yzerman', tag: $('#yzerman'), vidTag: $('#yzermanReel')},
-	{name: 'ovie', tag: $('#ovie'), vidTag: $('#ovieReel')},
-	{name: 'teemu', tag: $('#teemu'), vidTag: $('#teemuReel')}
+	{name: 'giroux', tag: $('#giroux'), vidTag: $('#girouxReel'), row: 'top'},
+	{name: 'wayne', tag: $('#wayne'), vidTag: $('#wayneReel'), row: 'top'},
+	{name: 'hasek', tag: $('#hasek'), vidTag: $('#hasekReel'), row: 'top'},
+	{name: 'dualPraise', tag: $('#dualPraise'), vidTag: $('#dualPraiseReel'), row: 'mid'},
+	{name: 'marty', tag: $('#marty'), vidTag: $('#martyReel'), row: 'mid'},
+	{name: 'roy', tag: $('#roy'), vidTag: $('#royReel'), row: 'mid'},
+	{name: 'yzerman', tag: $('#yzerman'), vidTag: $('#yzermanReel'), row: 'low'},
+	{name: 'ovie', tag: $('#ovie'), vidTag: $('#ovieReel'), row: 'low'},
+	{name: 'teemu', tag: $('#teemu'), vidTag: $('#teemuReel'), row: 'low'}
 ];
 
 $(document).ready(function () {
-	
+	//main handler for user interactions
 	$('.pics').on('click', function () {
 		let curr = this.id;
-		let position = arr.indexOf(arr[3]);
-		console.log(position);
-		//let currReel = '' + this.id + 'Reel';
-		
-		 for (let i in arr) {
-			
-			/*if (arr[i].name == curr || i == 3 || i == 6) {
+		console.log(curr);
+		for (let i in arr) {
+			if (curr == arr[i].name) {
+				//hide clicked picture and reveal video
 				arr[i].tag.addClass('hidden');
-				arr[i+1].tag.addClass('hidden');
-				arr[i+2].tag.addClass('hidden');
-			}
-			
-			if (i == 1 || i == 4 || i == 7) {
-				arr[i-1].tag.addClass('hidden');
-				arr[i].tag.addClass('hidden');
-				arr[i+1].tag.addClass('hidden');
-				
-			}
-			
-			if (i == 2 || i == 5 || i == 8) {
-				arr[i-2].tag.addClass('hidden');
-				arr[i-1].tag.addClass('hidden');
-				arr[i].tag.addClass('hidden');
-				
-			}*/
-			
-			 if (arr[i].name == this.id) {
-				arr[i].tag.addClass('hidden');
-				 if (arr[i].name == 'teemu') {
-					arr[7].tag.addClass('hidden');
-					arr[6].tag.addClass('hidden');
-				}
-				//console.log(arr[i+1].tag);
-			} 
-		} 
-		
-		//MAKE IT UNIVERSAL PER ROW
-		//BACK BUTTON GENERATES ROW SPECIFIC ALSO
-		
-		/* $('#wayne').addClass('hidden');
-		$('#hasek').addClass('hidden');
-		$('#giroux').addClass('hidden'); */
-		//$('#girouxReel').removeClass('hidden');
-		console.log(this.id);
-		
-		//MAKE RELATIVE BACK BUTTON VISIBLE
-		if (this.id == arr[0].name || this.id == arr[1].name || this.id == arr[2].name) {
-			$('#topRowReturn').removeClass('hidden');
-		} else if (this.id == arr[3].name || this.id == arr[4].name || this.id == arr[5].name) {
-			$('#midRowReturn').removeClass('hidden');
-		} else {
-			$('#lowRowReturn').removeClass('hidden');
-		}
-		
-		
-	});
+				arr[i].vidTag.removeClass('hidden');
+				let row = arr[i].row;
+				if (row == 'top') {
+					$('#topRowReturn').removeClass('hidden');
+					//hide the rest of the row
+					for (let j in arr) {
+						if (arr[j].row == row) {
+							arr[j].tag.addClass('hidden');
+						}
+					}
+				} else if (row == 'mid') {
+					$('#midRowReturn').removeClass('hidden');
+					//hide the rest of the row
+					for (let j in arr) {
+						if (arr[j].row == row) {
+							arr[j].tag.addClass('hidden');
+						}
+					}
+				} else if (row == 'low') {
+					$('#lowRowReturn').removeClass('hidden');
+					//hide the rest of the row
+					for (let j in arr) {
+						if (arr[j].row == row) {
+							arr[j].tag.addClass('hidden');
+						}
+					}
+				}//end of internal if statement
+			}//end of if statement
+		}//end of for loop
+	});//end of click function
 	
-	
+	//behavior of the return buttons
 	$('#topRowReturn').click(function () {
 		$('#topRow .pics').each(function () {
 			$(this).removeClass('hidden');
-			$('#girouxReel').addClass('hidden');
+		});
+		$('#topRow .vids').each(function () {
+			$(this).addClass('hidden');
 		});
 		$(this).addClass('hidden');
 	});
@@ -83,7 +64,9 @@ $(document).ready(function () {
 	$('#midRowReturn').click(function () {
 		$('#midRow .pics').each(function () {
 			$(this).removeClass('hidden');
-			$('#girouxReel').addClass('hidden');
+		});
+		$('#midRow .vids').each(function () {
+			$(this).addClass('hidden');
 		});
 		$(this).addClass('hidden');
 	});
@@ -91,10 +74,11 @@ $(document).ready(function () {
 	$('#lowRowReturn').click(function () {
 		$('#lowRow .pics').each(function () {
 			$(this).removeClass('hidden');
-			$('#girouxReel').addClass('hidden');
+		});
+		$('#lowRow .vids').each(function () {
+			$(this).addClass('hidden');
 		});
 		$(this).addClass('hidden');
 	});
-	
 	
 });
